0e3c22929e65b4f60fef2f5ea9f1e63b75137cfe
Sapsan <semya_s@mail.ru>
2022-12-28 23:51:38 +0300

    //Задача 47. Задайте двумерный массив размером m×n, //заполненный случайными вещественными числами. //m = 3, n = 4. //0,5 7 - 2 - 0,2 //1 - 3,3 8 - 9,9 //8 7,8 - 7,1 9

---
+++ Task47/TAsk47/TAsk47/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n,
+//заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 - 2 - 0,2
+//1 - 3,3 8 - 9,9
+//8 7,8 - 7,1 9
+double[,] inputTwoDimensionArray(int length, int secondLength)
+{
+    double[,] Array = new double[length, secondLength];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        FillDimension(i, Array);
+    }
+    return Array;
+}
+
+int ReadInt()
+{
+    Console.WriteLine("input number");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+void FillDimension(int index, double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(1); i++)
+    {
+        Array[index, i] = new Random().NextDouble()*100;
+    }
+}
+void GetArrayAsString(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Task();
+void Task()
+{
+    double[,] Array = inputTwoDimensionArray(ReadInt(), ReadInt());
+    GetArrayAsString(Array);
+}
